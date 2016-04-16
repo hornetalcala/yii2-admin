451313a377dea3a488cf94087679eadf8f9fbffe
carlos <carlos>
2016-04-16 21:21:15 +0200

    Initial Commit

---
+++ AnimateAsset.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace hornetalcala\admin;
+
+use yii\web\AssetBundle;
+
+/**
+ * Description of AnimateAsset
+ *
+ * @author Misbahul D Munir <misbahuldmunir@gmail.com>
+ * @since 2.5
+ */
+class AnimateAsset extends AssetBundle
+{
+    /**
+     * @inheritdoc
+     */
+    public $sourcePath = '@hornetalcala/admin/assets';
+    /**
+     * @inheritdoc
+     */
+    public $css = [
+        'animate.css',
+    ];
+
+}
